c180b7d4468c14227881ddf000ddbad14dd2bc61
SevenHolySwords <imrath.keith@gmail.com>
2025-04-02 23:10:49 -0400

    almost complete

--- src/screens/ArticleDetailScreen.js
+++ src/screens/ArticleDetailScreen.js
@@ -33,11 +33,32 @@ export default function ArticleDetailScreen(props) {
     >
       {/* Article Image */}
       <View style={styles.imageContainer} testID="imageContainer">
-         
+         <Image
+          source={{ uri: article.thumbnail }}
+          style={styles.articleImage}
+        />
       </View>
 
       {/* Back Button and Favorite Button */}
-                 
+                 <View style={styles.topButtonsContainer}>
+        <TouchableOpacity
+          onPress={() => navigation.goBack()}
+          style={styles.backButton}
+        >
+          <Text>Back</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={handleToggleFavorite}
+          style={[
+            styles.favoriteButton,
+            {
+              backgroundColor: "white",
+            },
+          ]}
+        >
+          <Text>{isFavourite ? "♥" : "♡"}</Text>
+        </TouchableOpacity>
+      </View> 
 
       {/* Article Description */}
   
@@ -49,11 +70,11 @@ export default function ArticleDetailScreen(props) {
           >
             <Text style={styles.articleTitle} testID="articleTitle">
          
-             
+                {article.title}
               
               </Text>
             <Text style={styles.articleCategory} testID="articleCategory">
-                         
+                    {article.category}     
               </Text>
           </View>
 
@@ -63,7 +84,9 @@ export default function ArticleDetailScreen(props) {
             style={styles.sectionContainer}
             testID="sectionContainer"
           >
-          
+                 <Text style={styles.sectionTitle}>Description</Text>
+            <Text style={styles.descriptionText}>{article.description}</Text>
+            
           </View>
         </View>
     </ScrollView>
